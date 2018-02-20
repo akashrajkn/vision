3541db07a3d9193d74384b7391559ab3d5c3e116
krasheninnikov <dmkr0001@gmail.com>
2018-02-20 20:37:03 +0100

    fixed

--- lab1/color_constancy/AWB.m
+++ lab1/color_constancy/AWB.m
@@ -1,31 +1,18 @@
-function awb (image_dir )
+function awb ( )
 
-if nargin == 0
-    image_dir = './';
-end
+im = im2double(imread('awb.jpg'));
+im_corrected = zeros(size(im));
+means_adj = [.5, .5, .5] ./ reshape(mean(mean(im)), [1, 3]);
 
-im = imread(fullfile(image_dir, 'awb.jpg'));
-im = double(im)
-
-im_corrected = im;
-chrom = im;
-for channel = 1:3
-    chrom(:, :, channel) = im(:, :, channel) ./ sum(im, 3) ;
-end
-
-chrom_corrected = chrom;
-for channel = 1:3
-    chrom_corrected(:, :, channel) = chrom(:, :, channel) / mean((mean(chrom(:, :, channel))));
-    im_corrected(:, :, channel) = chrom_corrected(:, :, channel) .* im(:, :, channel);
+for channel = [1:3]
+    im_corrected(:, :, channel) = im(:, :, channel) .*  means_adj(channel);
 end
 
 ax1 = subplot(1,2,1);
-imshow(uint8(im));
+imshow(im);
 title(ax1,'\fontsize{16}Original image')
 
 ax2 = subplot(1,2,2);
-imshow(uint8(im_corrected));
+imshow(im_corrected);
 title(ax2,'\fontsize{16}Corrected image')
-
-disp(mean((mean(chrom_corrected))))
 end
