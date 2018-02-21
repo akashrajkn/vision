5fa8c82e315bb2d795259902637df2488cf5f272
krasheninnikov <dmkr0001@gmail.com>
2018-02-21 17:43:32 +0100

    normals

--- lab1/photometric/plot_surface.m
+++ lab1/photometric/plot_surface.m
@@ -1,5 +1,10 @@
-function plot_surface( height_map )
+function plot_surface( normals, height_map )
 
 [X,Y] = meshgrid(1:16:512);
+Z = downsample(downsample(height_map, 16)', 16)';
 colormap default
-surf(X, Y, downsample(downsample(height_map, 16)', 16)')
+surf(X, Y, Z)
+
+V = normals(1:16:end, 1:16:end, :);
+disp([size(Z), size( V(:, :, 1))])
+quiver3(X, Y, Z, V(:, :, 1), V(:, :, 2), V(:, :, 3), 0.5)
