4bf12c7b441e9a3954252539d8e1480d44231fa7
krasheninnikov <dmkr0001@gmail.com>
2018-02-21 19:51:50 +0100

    subplots

--- lab1/photometric/plot_surface.m
+++ lab1/photometric/plot_surface.m
@@ -1,11 +1,18 @@
 function plot_surface( normals, height_map )
 
 scaling_factor = 16
+
 [X,Y] = meshgrid(1:scaling_factor:512);
 Z = downsample(downsample(height_map, scaling_factor)', scaling_factor)';
-colormap default
-surf(X, Y, Z)
-
 V = normals(1:scaling_factor:end, 1:scaling_factor:end, :);
-disp([size(Z), size( V(:, :, 1))])
+
+% Plot
+colormap default
+
+ax1 = subplot(1,2,1);
+surf(X, Y, Z)
+title(ax1,'\fontsize{14}Shape')
+
+ax2 = subplot(1,2,2);
 quiver3(X, Y, Z, V(:, :, 1), V(:, :, 2), V(:, :, 3), 0.5)
+title(ax2,'\fontsize{14}Normals')
