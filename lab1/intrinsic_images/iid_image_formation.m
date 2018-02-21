cb7c973c19937fb70b45a395e3b310d46709b878
krasheninnikov <dmkr0001@gmail.com>
2018-02-21 10:06:42 +0100

    clean code and comments

--- lab1/intrinsic_images/iid_image_formation.m
+++ lab1/intrinsic_images/iid_image_formation.m
@@ -1,29 +1,29 @@
-function iid_image_formation (image_dir )
+function iid_image_formation ()
+% IID_IMAGE_FORMATION reconstucts an image from its shading and reflectance
+% components.
 
-if nargin == 0
-    image_dir = './';
-end
+% load input images
+im_ball = imread('ball.png');
+im_reflectance = imread('ball_reflectance.png');
+im_shading = imread('ball_shading.png');
 
-% read input images
-im_ball = imread(fullfile(image_dir, 'ball.png'));
-im_reflectance = imread(fullfile(image_dir, 'ball_reflectance.png'));
-im_shading = imread(fullfile(image_dir, 'ball_shading.png'));
-
+% reconstruct the image
 im_reconstr = double(im_reflectance) .* double(im_shading);
 
-ax1 = subplot(1,4,1);
+% plotting
+ax1 = subplot(2,2,1);
 imshow(im_ball)
 title(ax1,'\fontsize{16}Original image')
 
-ax2 = subplot(1,4,2);
+ax2 = subplot(2,2,2);
 imshow(im_reflectance)
 title(ax2,'\fontsize{16}Reflectance component')
 
-ax3 = subplot(1,4,3);
+ax3 = subplot(2,2,3);
 imshow(im_shading)
 title(ax3,'\fontsize{16}Shading component')
 
-ax4 = subplot(1,4,4);
+ax4 = subplot(2,2,4);
 imshow(uint16(im_reconstr))
 title(ax4,'\fontsize{16}Reconstructed image')
 
