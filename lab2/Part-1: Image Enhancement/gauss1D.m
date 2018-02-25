904faa3fbceea8b096cfd9f2166886abcc7620a1
akashrajkn <akashrajkn@gmail.com>
2018-02-25 11:40:44 +0100

    Gaussian 1D initial

--- lab2/Part-1: Image Enhancement/gauss1D.m
+++ lab2/Part-1: Image Enhancement/gauss1D.m
@@ -3,5 +3,7 @@ function G = gauss1D( sigma , kernel_size )
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
-    %% solution
+
+    x_values = ceil(-kernel_size / 2):1:floor(kernel_size / 2);
+    G = 1 / (sigma * sqrt(2 * pi)) * exp(-x_values.^2 / (2 * sigma ^ 2));
 end
