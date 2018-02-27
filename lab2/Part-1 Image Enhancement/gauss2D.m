4ff1e58c92953cde95e08088036226f904961fd6
akashrajkn <akashrajkn@gmail.com>
2018-02-28 00:28:22 +0100

    2-D Gaussian filter implementation

--- lab2/Part-1 Image Enhancement/gauss2D.m
+++ lab2/Part-1 Image Enhancement/gauss2D.m
@@ -1,3 +1,13 @@
-function G = gauss2D( sigma , kernel_size )
-    %% solution
+function G = gauss2D(sigma, kernel_size)
+% 2-D Gaussian Filter - Uses the Gaussian 1-D filter implementation
+% @param sigma: variance of the Gaussian
+% @param kernel_size: size of the convolutional kernel
+%
+% @return G: 2-D Gaussian filter matrix
+
+    g_x = gauss1D(sigma, kernel_size);
+    g_y = gauss1D(sigma, kernel_size);
+
+    G = transpose(g_x) * g_y;
+
 end
