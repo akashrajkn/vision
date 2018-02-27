5b28b442e0c0b561628a6ddd17be0a48118ede74
akashrajkn <akashrajkn@gmail.com>
2018-02-28 00:28:08 +0100

    Normalized 1-D Gaussian filter

--- lab2/Part-1 Image Enhancement/gauss1D.m
+++ lab2/Part-1 Image Enhancement/gauss1D.m
@@ -1,9 +1,19 @@
-function G = gauss1D( sigma , kernel_size )
+function G = gauss1D(sigma, kernel_size)
+% 1-D Gaussian filter
+% @param sigma: variance of the Gaussian
+% @param kernel_size: size of the convolutional kernel
+%
+% @return G: 1-D Gaussian filter matrix
+
     G = zeros(1, kernel_size);
+
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
 
     x_values = ceil(-kernel_size / 2):1:floor(kernel_size / 2);
-    G = 1 / (sigma * sqrt(2 * pi)) * exp(-x_values.^2 / (2 * sigma ^ 2));
+    g = exp(-x_values.^2 / (2 * sigma ^ 2)) / (sigma * sqrt(2 * pi));
+
+    % normalize the filter
+    G = g ./ sum(g);
 end
