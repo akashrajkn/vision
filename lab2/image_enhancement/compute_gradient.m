980822364359a89a79510e267fabac08f2cea79d
krasheninnikov <dmkr0001@gmail.com>
2018-02-28 21:19:53 +0100

    upd

--- lab2/image_enhancement/compute_gradient.m
+++ lab2/image_enhancement/compute_gradient.m
@@ -1,5 +1,11 @@
 function [Gx, Gy, im_magnitude,im_direction] = compute_gradient(image)
-fprintf('Not implemented\n')
+sobel_x = reshape([1, 2, 1, 0, 0, 0, -1, -2, -1], [3, 3]);
+sobel_y =  reshape([1, 0, -1, 2, 0, -2, 1, 0, -1], [3 ,3]);
 
+Gx =  imfilter(image, sobel_x, 'conv');
+Gy =  imfilter(image, sobel_y, 'conv');
+
+im_magnitude = sqrt(Gx.^2 + Gy.^2);
+im_direction = atand(Gy./Gx);
 end
 
