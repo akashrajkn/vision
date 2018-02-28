67435f0c9800571da67bfd5b78796ef3a74ce2c2
krasheninnikov <dmkr0001@gmail.com>
2018-02-28 10:44:28 +0100

    rename

--- lab2/image_enhancement/compute_PSNR_question6.m
+++
@@ -1,9 +0,0 @@
-original_im = im2double(imread('./images/image1.jpg'));
-noisy_im_saltpepper = im2double(imread('./images/image1_saltpepper.jpg'));
-noisy_im_gauss = im2double(imread('./images/image1_gaussian.jpg'));
-
-fprintf('PSNR between image1_saltpepper.jpg and image1.jpg:\n')
-disp(myPSNR(original_im, noisy_im_saltpepper))
-
-fprintf('PSNR between image1_gaussian.jpg and image1.jpg:\n')
-disp(myPSNR(original_im, noisy_im_gauss))
