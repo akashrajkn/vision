866ea969bd158e4c5eacb5c301171c5566b817c5
akash raj <akashrajkn@gmail.com>
2018-03-13 10:48:59 +0100

    Keypoint matching - file initialized

---
+++ lab4/keypoint_matching.m
@@ -0,0 +1,10 @@
+function [matchings] = keypoint_matching(image1,image2)
+% Computes the keypoint matchings between the input images
+%   @param: image1
+%   @param: image2
+%
+%   @return: keypoint matchings
+
+    matchings = image1 + image2;
+end
+
