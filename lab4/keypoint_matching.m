79e65afa33684c01719e62ba4d8e7086ea522e68
akash raj <akashrajkn@gmail.com>
2018-03-14 10:14:09 +0100

    keypoint_matching function defintion changed

--- lab4/keypoint_matching.m
+++ lab4/keypoint_matching.m
@@ -1,4 +1,4 @@
-function [matchings] = keypoint_matching(image1, image2)
+function [matchings, f1, d1, f2, d2] = keypoint_matching(image1, image2)
 % Computes the keypoint matchings between the input images
 % NOTE: Requires VLFeat (http://www.vlfeat.org/)
 %   @param: image1
@@ -7,16 +7,12 @@ function [matchings] = keypoint_matching(image1, image2)
 %   @return: keypoint matchings
 
     if nargin < 2
-        image1 = imread('boat1.pgm');
-        image2 = imread('boat2.pgm');
+        image1 = single(imread('boat1.pgm'));
+        image2 = single(imread('boat2.pgm'));
     end
 
-    % convert to type single
-    boat1_s = single(boat1);
-    boat2_s = single(boat2);
-
-    [f1, d1] = vl_sift(boat1_s);
-    [f2, d2] = vl_sift(boat2_s);
+    [f1, d1] = vl_sift(image1);
+    [f2, d2] = vl_sift(image2);
 
     matchings = vl_ubcmatch(d1, d2);
 end
---
+++ lab4/main_q1.m
@@ -0,0 +1,45 @@
+% -------------------------- Params --------------------------
+samples = 50;
+% ------------------------------------------------------------
+
+% -------------------------- Images --------------------------
+image1 = imread('boat1.pgm');
+image2 = imread('boat2.pgm');
+
+if ndims(image1) == 3
+    image1 = rgb2gray(image1);
+end
+
+if ndims(image2) == 3
+    image2 = rgb2gray(image2);
+end
+
+% convert to type single
+image1_s = single(image1);
+image2_s = single(image2);
+% ------------------------------------------------------------
+
+% Keypoint matchings
+[matchings, f1, d1, f2, d2] = keypoint_matching(image1_s, image2_s);
+perm = randperm(size(matchings, 2));
+sel = perm(1:samples);
+matchings = matchings(:, sel);
+
+% -------------------------- Plots ---------------------------
+figure(1); clf;
+imshow(cat(2, image1, image2));
+
+x1 = f1(1, matchings(1, :));
+x2 = f2(1, matchings(2, :)) + size(image1_s, 2);
+y1 = f1(2, matchings(1, :));
+y2 = f2(2, matchings(2, :));
+
+hold on ;
+h = line([x1 ; x2], [y1 ; y2]);
+set(h, 'linewidth', 1, 'color', 'b');
+
+vl_plotframe(f1(:, matchings(1, :)));
+f2(1,:) = f2(1, :) + size(image1_s, 2);
+vl_plotframe(f2(:, matchings(2, :)));
+axis image off;
+% ------------------------------------------------------------
