1d2a8bbb023471ff824b3ce1cddf1784378ee882
akash raj <akashrajkn@gmail.com>
2018-03-14 09:58:57 +0100

    Keypoint matching function and test file

--- lab4/keypoint_matching.m
+++ lab4/keypoint_matching.m
@@ -1,10 +1,22 @@
-function [matchings] = keypoint_matching(image1,image2)
+function [matchings] = keypoint_matching(image1, image2)
 % Computes the keypoint matchings between the input images
+% NOTE: Requires VLFeat (http://www.vlfeat.org/)
 %   @param: image1
 %   @param: image2
 %
 %   @return: keypoint matchings
 
-    matchings = image1 + image2;
-end
+    if nargin < 2
+        image1 = imread('boat1.pgm');
+        image2 = imread('boat2.pgm');
+    end
 
+    % convert to type single
+    boat1_s = single(boat1);
+    boat2_s = single(boat2);
+
+    [f1, d1] = vl_sift(boat1_s);
+    [f2, d2] = vl_sift(boat2_s);
+
+    matchings = vl_ubcmatch(d1, d2);
+end
