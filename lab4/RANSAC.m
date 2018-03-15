6da59624317ea693a91a2db531adaf580ffe83dc
krasheninnikov <dmkr0001@gmail.com>
2018-03-15 12:52:30 +0100

    ransac function

--- lab4/RANSAC.m
+++ lab4/RANSAC.m
@@ -1,8 +1,10 @@
-function [T] = RANSAC(matchings, f1, f2)
+function [T] = RANSAC(matchings, f1, f2, N, P)
 % Returns a transformation
 %   @param matchings - keypoint matchings
 %   @param f1
 %   @param f2
+%   @param N - number of iterations for RANSAC
+%   @param P - subset of matchings to use in RANSAC
 %
 %   @return: transformation T
 
@@ -11,10 +13,65 @@ function [T] = RANSAC(matchings, f1, f2)
     x2 = f2(1, matchings(2, :));
     y2 = f2(2, matchings(2, :));
 
-    x1 = x1';
-    y1 = y1';
-    x2 = x2';
-    y2 = y2';
+    x1_t = x1';
+    y1_t = y1';
+
+    zer = zeros(size(x1_t));
+    one = ones(size(x1_t));
+    A_n = [x1_t y1_t zer zer one zer; zer zer x1_t y1_t zer one];
+    num_p = size(matchings, 2);  % number of points
+    
+    best_transformation = [];
+    best_count = 0;
+
+    for iteration = 1:N
+        % sample P points
+        perm = randperm(size(matchings, 2));
+        sel = perm(1:P);
+        matchings_p = matchings(:, sel);
+        T = compute_transformation(matchings_p, f1, f2);
+        new_points = A_n * T;
+
+        % count inliers;
+        count = 0;
+        for i = 1:num_p
+            p1_x = new_points(i);
+            p1_y = new_points(num_p + i);
+
+            p2_x = x2(i);
+            p2_y = y2(i);
+
+            dist = sqrt((p1_x - p2_x) ^ 2 + (p1_y - p2_y) ^ 2);  % Use Euclidean distance
+
+            if dist <= 10
+                count = count + 1;
+            end
+        end
+
+        if count > best_count
+            best_count = count;
+            best_transformation = T;
+        end
+    end
+
+    disp("Best transformation is: ")
+    disp(best_transformation)
+
+end
+
+
+function [T] = compute_transformation(matchings, f1, f2)
+% Returns a transformation
+%   @param matchings - keypoint matchings
+%   @param f1
+%   @param f2
+%
+%   @return: transformation T
+
+    x1 = f1(1, matchings(1, :))';
+    y1 = f1(2, matchings(1, :))';
+    x2 = f2(1, matchings(2, :))';
+    y2 = f2(2, matchings(2, :))';
 
     zer = zeros(size(x1));
     one = ones(size(x1));
@@ -25,3 +82,4 @@ function [T] = RANSAC(matchings, f1, f2)
     T = pinv(A) * b;
 
 end
+
--- lab4/main_q1.m
+++ lab4/main_q1.m
@@ -1,5 +1,5 @@
-clear all
-clc
+clear all;
+clc;
 
 % -------------------------- Params --------------------------
 samples = 50;  % number of matchings to take
@@ -34,56 +34,8 @@ matchings = matchings(:, sel);
 plot_descriptors(image1, image2, matchings, f1, f2)
 % ------------------------------------------------------------
 
-% Question 3: Find best transformation
-x1 = f1(1, matchings(1, :));
-y1 = f1(2, matchings(1, :));
-x2 = f2(1, matchings(2, :));
-y2 = f2(2, matchings(2, :));
-
-x1_t = x1';
-y1_t = y1';
-
-zer = zeros(size(x1_t));
-one = ones(size(x1_t));
-A_n = [x1_t y1_t zer zer one zer; zer zer x1_t y1_t zer one];
-num_p = size(matchings, 2);  % number of points
-
-best_transformation = [];
-best_count = 0;
-
-for iteration = 1:N
-    % sample P points
-    perm = randperm(size(matchings, 2));
-    sel = perm(1:P);
-    matchings_p = matchings(:, sel);
-
-    T = RANSAC(matchings_p, f1, f2);
-    new_points = A_n * T;
-
-    % count inliers;
-    count = 0;
-    for i = 1:num_p;
-        p1_x = new_points(i);
-        p1_y = new_points(num_p + i);
-
-        p2_x = x2(i);
-        p2_y = y2(i);
-
-        dist = sqrt((p1_x - p2_x) ^ 2 + (p1_y - p2_y) ^ 2);  % Use Euclidean distance
-
-        if dist <= 10
-            count = count + 1;
-        end
-    end
-
-    if count > best_count
-        best_count = count;
-        best_transformation = T;
-    end
-end
-
-disp("Best transformation is: ")
-disp(best_transformation)
+% Question 3: Find best transformation with RANSAC
+best_transformation = RANSAC(matchings, f1, f2, N, P);
 
 % -------------------------- Plots ---------------------------
 plot_ransac(image1, image2, best_transformation)
