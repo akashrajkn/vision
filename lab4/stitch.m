c05b59f6f34a75e969b064fef7e48df9d842344f
krasheninnikov <dmkr0001@gmail.com>
2018-03-15 23:33:09 +0100

    color; todo: transform w/o dots

--- lab4/stitch.m
+++ lab4/stitch.m
@@ -1,17 +1,17 @@
 clear all; close all; clc;
 
 
-im1 = imread('left.jpg');
-im2 = imread('right.jpg');
+im1_original = imread('left.jpg');
+im2_original = imread('right.jpg');
 
 channels = 1;
-if ndims(im1) == 3
-    [~, ~, channels] = size(im1);
-    im1 = rgb2gray(im1);
+if ndims(im1_original) == 3
+    [~, ~, channels] = size(im1_original);
+    im1 = rgb2gray(im1_original);
 end
 
-if ndims(im2) == 3
-    im2 = rgb2gray(im2);
+if ndims(im2_original) == 3
+    im2 = rgb2gray(im2_original);
 end
 
 % convert to type single
@@ -20,27 +20,24 @@ im2_s = single(im2);
 
 [matchings, f1, d1, f2, d2] = keypoint_matching(im1_s, im2_s);
 T = RANSAC(matchings, f1, f2, 10000, 5);
-%T = T.*[1, 1, 1, 1, 0, 0]'
-%T = T.*[1, -1, -1, 1, -1, -1]';
-im2_tr = transform_image(im2, T, 'none');
+im2_tr = transform_image(im2_original, T, 'none');
 
 [h1, w1] = size(im1);
 [h2, w2] = size(im2);
-[h2_tr, w2_tr] = size(im2_tr);
+[h2_tr, w2_tr, ~] = size(im2_tr);
 
-h_stitched = max(h1, h2_tr)
-w_stitched = round(w2_tr - T(5) + 1)
-im_stitched = zeros(h_stitched, w_stitched);
+h_stitched = max(h1, h2_tr);
+w_stitched = round(w2_tr - T(5) + 1);
+im_stitched = zeros(h_stitched, w_stitched, channels);
 
 % TODO expand im1 just in case
-im_stitched(:, 1:w1) = im1;
+im_stitched(:, 1:w1, :) = im1_original;
 
-
-h_shift = round(- T(6) +  h2 - h_stitched + 1 )
-w_shift = round(- T(5) + 1)
+h_shift = round(- T(6) +  h2 - h_stitched + 1 );
+w_shift = round(- T(5) + 1);
 
 % stitching with original im1 on top
-im_stitched(h_shift:h_shift+h2_tr-1, w1:w_shift+w2_tr-1) = im2_tr(:, w1-w_shift+1:end);
+im_stitched(h_shift:h_shift+h2_tr-1, w1:w_shift+w2_tr-1, :) = im2_tr(:, w1-w_shift+1:end, :);
 
 % stitching with transformed im2 on top
 % for i = 1:h2_tr
@@ -51,7 +48,8 @@ im_stitched(h_shift:h_shift+h2_tr-1, w1:w_shift+w2_tr-1) = im2_tr(:, w1-w_shift+
 %     end
 % end
 
-subplot(1,4,1), imshow(im1)
-subplot(1,4,2), imshow(im2)
-subplot(1,4,3), imshow(uint8(im2_tr))
-subplot(1,4,4), imshow(uint8(im_stitched))
+% subplot(1,4,1), imshow(im1_original)
+% subplot(1,4,2), imshow(im2_original)
+% subplot(1,4,3), imshow(uint8(im2_tr))
+% subplot(1,4,4), 
+imshow(uint8(im_stitched))
--- lab4/transform_image.m
+++ lab4/transform_image.m
@@ -1,21 +1,21 @@
 function [image_transformed] = transform_image(image, T, filter)
 
-    [h,w] = size(image);
+    [h,w,channels] = size(image);
     M = reshape(T(1:4), [2,2]);
     corners = round([1,1; h,1; 1,w; h,w] * M)
 
     h_new = max(corners(2,1), corners(4,1)) - min(corners(1,1), corners(3,1)) + 1;
     w_new = max(corners(3,2), corners(4,2)) - min(corners(1,2), corners(2,2)) + 1;
 
-    offset_h = 1-min(corners(:,1))
-    offset_w = 1-min(corners(:,2))
+    offset_h = 1-min(corners(:,1));
+    offset_w = 1-min(corners(:,2));
 
-    image_transformed = zeros(h_new, w_new);
+    image_transformed = zeros(h_new, w_new, channels);
 
     for i = 1:h
         for j = 1:w
             new_point = round([i,j]*M) + [offset_h, offset_w];
-            image_transformed(new_point(1), new_point(2)) = image(i, j);
+            image_transformed(new_point(1), new_point(2), :) = image(i, j, :);
         end
     end
     if filter == 'medi'
