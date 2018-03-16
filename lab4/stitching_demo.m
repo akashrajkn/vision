84da1cdf5a82131cfba62b1b8057ee6b7addf601
krasheninnikov <dmkr0001@gmail.com>
2018-03-16 19:48:07 +0100

    subplots of originals

--- lab4/stitching_demo.m
+++ lab4/stitching_demo.m
@@ -7,5 +7,8 @@ im2 = imread('right.jpg');
 % stitch the images
 im_stitched = stitch(im1, im2);
 
-% plot the resulting stitched image
-imshow(uint8(im_stitched))
+% plot the original images and the resulting stitched image
+subplot(1,2,1); imshow(im1);
+subplot(1,2,2); imshow(im2);
+
+figure(); imshow(uint8(im_stitched))
